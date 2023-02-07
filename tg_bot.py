9e4ec31ecfe9765b8e11651c2ad58368fada797a
DarkWerb22 <100151269+DarkWerb22@users.noreply.github.com>
2023-02-07 10:24:57 +0200

    добавил авто-отправку фотографий

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,18 @@
 import telegram
 from dotenv import load_dotenv
+from time import sleep
+import random
 import os
 if __name__=="__main__":
     load_dotenv()
     tg_token = os.getenv("TG_BOT_TOKEN")
     chat_id = os.getenv("TG_CHAT_ID")
     bot = telegram.Bot(token=tg_token)
-    photo_path = "images/1nasa_epic.png"
-    with open(photo_path, 'rb') as photo:
-        bot.send_photo(chat_id, photo=photo)
+    files_in_dir = os.listdir("images")
+    sending_delay = os.getenv("SENDING_DELAY", "14400")
+    while True:
+        random_photo = random.choice(files_in_dir)
+        photo_path = f"images/{random_photo}"
+        with open(photo_path, 'rb') as photo:
+            bot.send_photo(chat_id, photo=photo)
+        sleep(int(sending_delay))
