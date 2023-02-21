929751fb6566174a6e9a6c8176cf39e8552cdc97
DarkWerb22 <100151269+DarkWerb22@users.noreply.github.com>
2023-02-21 10:29:28 +0200

    Сделал скрипт устойчивым к разрыву соединения

--- tg_bot.py
+++ tg_bot.py
@@ -3,16 +3,22 @@ from dotenv import load_dotenv
 from time import sleep
 import random
 import os
+
+
 if __name__=="__main__":
     load_dotenv()
-    tg_token = os.getenv("TG_BOT_TOKEN")
-    chat_id = os.getenv("TG_CHAT_ID")
+    tg_token = os.environ["TG_BOT_TOKEN"]
+    chat_id = os.environ["TG_CHAT_ID"]
     bot = telegram.Bot(token=tg_token)
     files_in_dir = os.listdir("images")
     sending_delay = os.getenv("SENDING_DELAY", "14400")
     while True:
         random_photo = random.choice(files_in_dir)
         photo_path = os.path.join("images", f"{random_photo}")
-        with open(photo_path, 'rb') as photo:
-            bot.send_photo(chat_id, photo=photo)
-        sleep(int(sending_delay))
+        try:
+            with open(photo_path, 'rb') as photo:
+                bot.send_photo(chat_id, photo=photo)
+            sleep(int(sending_delay))
+        except telegram.error.NetworkError:
+            print("Ошибка подлкючения телеграм")
+            sleep(15)
